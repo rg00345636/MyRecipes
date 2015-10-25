35ad4acac59fea58b60b32180ad2bf286e339884
rg00345636 <rg00345636@techmahindra.com>
2015-10-25 07:46:51 +0000

    Initialize repo and build home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :articles do
-    resources :comments
-  end
+ # resources :articles do
+ #   resources :comments
+ # end
  
-  root 'welcome#index'
+ # root 'welcome#index'
 
- 
+ get '/home', to: 'pages#home'
 end
